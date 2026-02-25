7937af1cb25c0b6ae8884c88f1610519f06366c8
Manikandan V <manikandan.v.2024.aids@rajalakshmi.edu.in>
2026-02-25 08:56:29 +0000

    basic funtionality working

---
+++ posts/first-post.md
@@ -0,0 +1,7 @@
+---
+title: "My First Post"
+date: "2026-02-25"
+---
+
+This is my first blog post.
+Welcome to my blog.
---
+++ src/lib/posts.js
@@ -0,0 +1,23 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+export function getAllPosts() {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  return fileNames.map((fileName) => {
+    const slug = fileName.replace(".md", "");
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+    const { data, content } = matter(fileContents);
+
+    return {
+      slug,
+      title: data.title,
+      date: data.date,
+      content,
+    };
+  });
+}
